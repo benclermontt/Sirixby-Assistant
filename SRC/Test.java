6e0e3851f7511ff93d44bba6043387165633c0e1
maxhrioux <45040811+maxhrioux@users.noreply.github.com>
2018-11-27 13:36:59 -0500

    Made it so the crawler only prints the first 10 results

--- SRC/Test.java
+++ SRC/Test.java
@@ -53,32 +53,27 @@ public class Test {
 
                  
             //Taking search term input from console
-        System.out.println("Please enter the search term.");
-        String searchTerm = userInput.nextLine();
-        System.out.println("Please enter the number of results. Example: 5 10 20");
-        int num = userInput.nextInt();
+             System.out.println("Please enter the search term.");
+             String searchTerm = userInput.nextLine();
 
-        String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+num;
+             String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+10;
         //without proper User-Agent, we will get 403 error
-        Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
-
-        //below will print HTML data, save it to a file and open in browser to compare
-        //System.out.println(doc.html());
+            Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
 
         //If google search results HTML change the <h3 class="r" to <h3 class="r1"
         //we need to change below accordingly
-        Elements results = doc.select("h3.r > a");
+            Elements results = doc.select("h3.r > a");
+            for (Element result : results) {
+                String linkHref = result.attr("href");
+                String linkText = result.text();
+                System.out.println(linkText + ", " + linkHref.substring(6, linkHref.indexOf("&")));
+             }
 
-        for (Element result : results) {
-            String linkHref = result.attr("href");
-            String linkText = result.text();
-            System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
-        }
 
         //We should try and have the user pick an option and then we should try and scrape the text off
         //of their choice and print it out for them
-        System.out.println("Which option would you like read out: ");
-        int choice = userInput.nextInt();
+            System.out.println("Which option would you like read out: ");
+            int choice = userInput.nextInt();
 
     }
 }
