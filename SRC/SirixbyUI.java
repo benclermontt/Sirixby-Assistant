c31bec839d11ff4f7d7a5130bcf4f3e21a711669
GoldrickD <37813134+GoldrickD@users.noreply.github.com>
2018-12-02 22:24:20 -0500

    OOF

---
+++ SRC/SirixbyUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class SirixbyUI extends JFrame{
+    private JPanel rootPanel;
+    private JTextField OutputNorth;
+    private JTextField InputSouth;
+
+
+    public SirixbyUI(){
+
+        add(rootPanel);
+
+        setTitle("Sirixby Personal Assistant");
+        setSize(400,500);
+    }
+
+}
